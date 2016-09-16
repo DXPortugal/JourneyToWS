e788696d18f0ec1cbaeb2f47cfa5c2f01de9d515
João Carlos Santos <Joo Carlos Santos>
2016-09-16 15:55:30 +0100

    Added ability to save masterclasses to calendar.

---
+++ wwwroot/js/calendar.js
@@ -0,0 +1,61 @@
+var b23s = ics();
+b23s.addEvent('Mobile Growth what works and what doesn\'t work', 'http://aka.ms/jwsmobilegrowth', 'Watch the event on Channel9!', '9/23/2016 3:00 pm', '9/23/2016 4:00 pm');
+var b25s = ics();
+b25s.addEvent('How to brand and communicate', 'http://aka.ms/jwshowtobrand', 'Watch the event on Channel9!', '9/25/2016 3:00 pm', '9/25/2016 4:00 pm');
+var b26s = ics();
+b26s.addEvent('Investment 101', 'http://aka.ms/jwsinvestment101', 'Watch the event on Channel9!', '9/26/2016 4:00 pm', '9/26/2016 5:00 pm');
+var t28s = ics();
+t28s.addEvent('Deploying Rancher on Azure', 'http://aka.ms/jwsrancheronazure', 'Watch the event on Channel9!', '9/28/2016 3:00 pm', '9/28/2016 4:00 pm');
+var t29s = ics();
+t29s.addEvent('Building your first bot with Microsoft Bot Framework and LUIS', 'http://aka.ms/jwsbotframework', 'Watch the event on Channel9!', '9/29/2016 3:00 pm', '9/29/2016 4:00 pm');
+var t4o = ics();
+t4o.addEvent('Unleashing the power of IoT', 'http://aka.ms/jwspowerofiot', 'Watch the event on Channel9!', '10/4/2016 3:00 pm', '10/4/2016 4:00 pm');
+var t6o = ics();
+t6o.addEvent('Using containers on Azure', 'http://aka.ms/asd', 'Watch the event on Channel9!', '10/6/2016 3:00 pm', '10/6/2016 4:00 pm');
+var t10o = ics();
+t10o.addEvent('How to build creative and usable websites', 'http://aka.ms/jwscreativewebsites', 'Watch the event on Channel9!', '10/10/2016 3:00 pm', '10/10/2016 4:00 pm');
+var t13o = ics();
+t13o.addEvent('Service Fabric and actor model architectures', 'http://aka.ms/jwsservicefabric', 'Watch the event on Channel9!', '10/13/2016 3:00 pm', '10/13/2016 4:00 pm');
+var t17o = ics();
+t17o.addEvent('Building Cognitive Solutions', 'http://aka.ms/jwscognitivesolutions', 'Watch the event on Channel9!', '10/17/2016 3:00 pm', '10/17/2016 4:00 pm');
+var t18o = ics();
+t18o.addEvent('From Modern WebStandards to Progressive Web APPs', 'http://aka.ms/asd', 'Watch the event on Channel9!', '10/18/2016 3:00 pm', '10/18/2016 4:00 pm');
+var t21o = ics();
+t21o.addEvent('Agile Project Management', 'http://aka.ms/jwsagile', 'Watch the event on Channel9!', '10/21/2016 3:00 pm', '10/21/2016 4:00 pm');
+
+$("#b23s").click(function() {
+    b23s.download();
+});
+$("#b25s").click(function() {
+    b25s.download();
+});
+$("#b26s").click(function() {
+    b26s.download();
+});
+$("#t28s").click(function() {
+    t28s.download();
+});
+$("#t29s").click(function() {
+    t29s.download();
+});
+$("#t4o").click(function() {
+    t4o.download();
+});
+$("#t6o").click(function() {
+    t6o.download();
+});
+$("#t10o").click(function() {
+    t10o.download();
+});
+$("#t13o").click(function() {
+    t13o.download();
+});
+$("#t17o").click(function() {
+    t17o.download();
+});
+$("#t18o").click(function() {
+    t18o.download();
+});
+$("#t21o").click(function() {
+    t21o.download();
+});
--- wwwroot/js/mail.js
+++ wwwroot/js/mail.js
@@ -1,11 +1,9 @@
-function ContactUs()
-{
-location.replace ('mailto:thejourney@microsoft.com?subject=[Journey to the Web Summit - Registration]&body=Hey,%0D%0A%0D%0AI would like my my startup to join the challenge.%0D%0AHere is our info:%0D%0A');
+function ContactUs() {
+    location.replace('mailto:thejourney@microsoft.com?subject=[Journey to the Web Summit - Registration]&body=Hey,%0D%0A%0D%0AI would like my my startup to join the challenge.%0D%0AHere is our info:%0D%0A');
 }
 
-function Help()
-{
-location.replace ('mailto:thejourney@microsoft.com?subject=[Journey to the Web Summit - Help]&body=Hey,%0D%0A%0D%0AMy name is <name> from <company_name>. I have a question for you guys:');
+function Help() {
+    location.replace('mailto:thejourney@microsoft.com?subject=[Journey to the Web Summit - Help]&body=Hey,%0D%0A%0D%0AMy name is <name> from <company_name>. I have a question for you guys:');
 }
 
 var links = document.getElementsByClassName('contactus');
