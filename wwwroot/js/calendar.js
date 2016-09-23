e73733e8c9a7d1305dd7e28032606ddd097c1b7d
João Carlos Santos <Joo Carlos Santos>
2016-09-23 11:55:18 +0100

    Added new masterclasses

--- wwwroot/js/calendar.js
+++ wwwroot/js/calendar.js
@@ -1,61 +1,83 @@
 var b23s = ics();
 b23s.addEvent('Mobile Growth what works and what doesn\'t work', 'http://aka.ms/jwsmobilegrowth', 'Watch the event on Channel9!', '9/23/2016 3:00 pm', '9/23/2016 4:00 pm');
-var b25s = ics();
-b25s.addEvent('How to brand and communicate', 'http://aka.ms/jwshowtobrand', 'Watch the event on Channel9!', '9/27/2016 3:00 pm', '9/27/2016 4:00 pm');
-var b26s = ics();
-b26s.addEvent('Investment 101', 'http://aka.ms/jwsinvestment101', 'Watch the event on Channel9!', '9/26/2016 4:00 pm', '9/26/2016 5:00 pm');
-var t28s = ics();
-t28s.addEvent('Deploying Rancher on Azure', 'http://aka.ms/jwsrancheronazure', 'Watch the event on Channel9!', '9/28/2016 3:00 pm', '9/28/2016 4:00 pm');
-var t29s = ics();
-t29s.addEvent('Building your first bot with Microsoft Bot Framework and LUIS', 'http://aka.ms/jwsbotframework', 'Watch the event on Channel9!', '9/29/2016 3:00 pm', '9/29/2016 4:00 pm');
-var t4o = ics();
-t4o.addEvent('Unleashing the power of IoT', 'http://aka.ms/jwspowerofiot', 'Watch the event on Channel9!', '10/4/2016 3:00 pm', '10/4/2016 4:00 pm');
-var t6o = ics();
-t6o.addEvent('Using containers on Azure', 'http://aka.ms/jwscontainersazure', 'Watch the event on Channel9!', '10/6/2016 3:00 pm', '10/6/2016 4:00 pm');
-var t10o = ics();
-t10o.addEvent('How to build creative and usable websites', 'http://aka.ms/jwscreativewebsites', 'Watch the event on Channel9!', '10/10/2016 3:00 pm', '10/10/2016 4:00 pm');
-var t13o = ics();
-t13o.addEvent('Service Fabric and actor model architectures', 'http://aka.ms/jwsservicefabric', 'Watch the event on Channel9!', '10/13/2016 3:00 pm', '10/13/2016 4:00 pm');
-var t17o = ics();
-t17o.addEvent('Building Cognitive Solutions', 'http://aka.ms/jwscognitivesolutions', 'Watch the event on Channel9!', '10/17/2016 3:00 pm', '10/17/2016 4:00 pm');
-var t18o = ics();
-t18o.addEvent('From Modern WebStandards to Progressive Web APPs', 'http://aka.ms/asd', 'Watch the event on Channel9!', '10/18/2016 3:00 pm', '10/18/2016 4:00 pm');
-var t21o = ics();
-t21o.addEvent('Agile Project Management', 'http://aka.ms/jwsagile', 'Watch the event on Channel9!', '10/21/2016 3:00 pm', '10/21/2016 4:00 pm');
-
 $("#b23s").click(function() {
     b23s.download();
 });
-$("#b25s").click(function() {
-    b25s.download();
-});
+
+var b26s = ics();
+b26s.addEvent('Investment 101', 'http://aka.ms/jwsinvestment101', 'Watch the event on Channel9!', '9/26/2016 4:00 pm', '9/26/2016 5:00 pm');
 $("#b26s").click(function() {
     b26s.download();
 });
+
+var b27s = ics();
+b27s.addEvent('How to brand and communicate', 'http://aka.ms/jwshowtobrand', 'Watch the event on Channel9!', '9/27/2016 3:00 pm', '9/27/2016 4:00 pm');
+$("#b27s").click(function() {
+    b27s.download();
+});
+
+var b14o = ics();
+b14o.addEvent('Crowd Funding', 'http://aka.ms/jwscrowdfunding', 'Watch the event on Channel9!', '10/14/2016 3:00 pm', '10/14/2016 4:00 pm');
+$("#b14o").click(function() {
+    b14o.download();
+});
+
+var b20o = ics();
+b20o.addEvent('Intellectual Property Rights (IPR)', 'http://aka.ms/jwsipr', 'Watch the event on Channel9!', '10/20/2016 3:00 pm', '10/20/2016 4:00 pm');
+$("#b20o").click(function() {
+    b20o.download();
+});
+
+var b21o = ics();
+b21o.addEvent('Agile Project Management', 'http://aka.ms/jwsagile', 'Watch the event on Channel9!', '10/21/2016 3:00 pm', '10/21/2016 4:00 pm');
+$("#b21o").click(function() {
+    b21o.download();
+});
+
+var t28s = ics();
+t28s.addEvent('Deploying Rancher on Azure', 'http://aka.ms/jwsrancheronazure', 'Watch the event on Channel9!', '9/28/2016 3:00 pm', '9/28/2016 4:00 pm');
 $("#t28s").click(function() {
     t28s.download();
 });
+
+var t29s = ics();
+t29s.addEvent('Building your first bot with Microsoft Bot Framework and LUIS', 'http://aka.ms/jwsbotframework', 'Watch the event on Channel9!', '9/29/2016 3:00 pm', '9/29/2016 4:00 pm');
 $("#t29s").click(function() {
     t29s.download();
 });
+
+var t4o = ics();
+t4o.addEvent('Unleashing the power of IoT', 'http://aka.ms/jwspowerofiot', 'Watch the event on Channel9!', '10/4/2016 3:00 pm', '10/4/2016 4:00 pm');
 $("#t4o").click(function() {
     t4o.download();
 });
+
+var t6o = ics();
+t6o.addEvent('Using containers on Azure', 'http://aka.ms/jwscontainersazure', 'Watch the event on Channel9!', '10/6/2016 3:00 pm', '10/6/2016 4:00 pm');
 $("#t6o").click(function() {
     t6o.download();
 });
+
+var t10o = ics();
+t10o.addEvent('How to build creative and usable websites', 'http://aka.ms/jwscreativewebsites', 'Watch the event on Channel9!', '10/10/2016 3:00 pm', '10/10/2016 4:00 pm');
 $("#t10o").click(function() {
     t10o.download();
 });
+
+var t13o = ics();
+t13o.addEvent('Service Fabric and actor model architectures', 'http://aka.ms/jwsservicefabric', 'Watch the event on Channel9!', '10/13/2016 3:00 pm', '10/13/2016 4:00 pm');
 $("#t13o").click(function() {
     t13o.download();
 });
+
+var t17o = ics();
+t17o.addEvent('Building Cognitive Solutions', 'http://aka.ms/jwscognitivesolutions', 'Watch the event on Channel9!', '10/17/2016 3:00 pm', '10/17/2016 4:00 pm');
 $("#t17o").click(function() {
     t17o.download();
 });
+
+var t18o = ics();
+t18o.addEvent('From Modern WebStandards to Progressive Web APPs', 'http://aka.ms/jwsmodernweb', 'Watch the event on Channel9!', '10/18/2016 3:00 pm', '10/18/2016 4:00 pm');
 $("#t18o").click(function() {
     t18o.download();
-});
-$("#t21o").click(function() {
-    t21o.download();
 });
